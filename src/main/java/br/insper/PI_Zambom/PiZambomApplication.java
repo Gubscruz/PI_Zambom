3d9de91453a72a7ad615ce484b64880de6c574d7
Gubscruz <124577208+Gubscruz@users.noreply.github.com>
2024-09-22 17:42:36 -0300

    commit

---
+++ src/main/java/br/insper/PI_Zambom/PiZambomApplication.java
@@ -0,0 +1,13 @@
+package br.insper.PI_Zambom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiZambomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiZambomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/insper/PI_Zambom/PiZambomApplicationTests.java
@@ -0,0 +1,13 @@
+package br.insper.PI_Zambom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiZambomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
